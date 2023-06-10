ce16d07c254803720be8e0e1772d0bd5ae00c46c
Jeremy Christian <jeremy.l.christian@gmail.com>
2023-06-10 12:16:56 -0700

    Finish exercise nine.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reverseString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return reverseString.split("").reverse().join("") == reverseString;
 };
 
 // Do not edit below this line
