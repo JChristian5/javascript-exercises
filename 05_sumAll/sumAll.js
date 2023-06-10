259a9763abb05f1abfc256a55366f262ed88ffc9
Jeremy Christian <jeremy.l.christian@gmail.com>
2023-06-10 11:41:54 -0700

    Finish exercises one through seven.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let num = 0
+    
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    } else if (typeof start === 'string' || typeof end === 'string') {
+        return 'ERROR';
+    } else if (typeof start === 'object' || typeof end === 'object') {
+        return 'ERROR';
+    } else if (start < end) {
+        for (let i = start; i <= end; i++) {
+            num += i;
+        }
 
+        return num;
+    } else if (start > end) {
+        for (let i = end; i <= start; i++) {
+            num += i;
+        }
+
+        return num;
+    } 
+    
 };
 
 // Do not edit below this line
