9f4cb199eca97429fe63652ffb1e07e8f3edad78
Jeremy Christian <jeremy.l.christian@gmail.com>
2023-06-11 11:37:43 -0700

    Finish exercise ten.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(count) {
+    if (count < 0) {
+        return 'OOPS';
+    }
+    const fibonacciNumber = [0, 1];
+    for (let i = 1; i < count; i++) {
+        fibonacciNumber.push(fibonacciNumber[i] + fibonacciNumber[i - 1]);
+    }
+    return fibonacciNumber[count];
 };
 
 // Do not edit below this line
